0ff565f05845fe110fe849397c428b52edd3c414
Apurva Kumar Sinha <ApurvaSinha17@users.noreply.github.com>
2015-01-05 20:26:14 -0500

    Update FileWrite.java

--- Bowling/src/bowling/FileWrite.java
+++ Bowling/src/bowling/FileWrite.java
@@ -6,14 +6,21 @@ import java.io.IOException;
 import java.io.PrintStream;
 
 public class FileWrite {
+	
+	private File file;
 
-	void fileWrite(String fName) throws IOException{
+	public void fileWrite(String fName) throws IOException{
 		
-		File file = new File("OutputFiles//"+fName);
+		file = new File("OutputFiles//"+fName);
 		FileOutputStream fos = new FileOutputStream(file);
 		PrintStream ps = new PrintStream(fos);
 		System.setOut(ps);
 	    
+	}
+	
+	public void fileClose(){
+		
+		file.close();
 	}
 	
 }
